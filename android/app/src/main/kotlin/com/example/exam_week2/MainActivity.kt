07ff226b79e35f9ebef38699040ad8693142d18d
shahincse2 <shahinalam.cse2@gmail.com>
2024-08-02 22:54:48 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/exam_week2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.exam_week2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  mySnackBar(message, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Greeting App'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Hello, World!',
+                style:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Welcome to Flutter!',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 200,
+                width: 200,
+                child: Image.network(
+                    'https://iconape.com/wp-content/png_logo_vector/flutter-logo.png'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Builder(builder: (context) {
+                return ElevatedButton(
+                  onPressed: () {
+                   mySnackBar('Pressed Me!', context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  child: const Text('Press Me'),
+                );
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
